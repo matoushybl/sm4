3219f9f239eeef2da80fe49f862d6ac76c8ee74d
Matous Hybl <hyblmatous@gmail.com>
2021-03-11 18:37:09 +0100

    Improve encoder API, start writing tests for it. #2

--- Software/sm4-shared/src/encoder.rs
+++ Software/sm4-shared/src/encoder.rs
@@ -5,14 +5,17 @@
 /// The encoder shall store information about the total position.
 /// The total position shall be resettable.
 /// For non-quadrature encoders a method that indicates change of motor rotation shall be implemented.
+use core::ops::{AddAssign, SubAssign};
+use embedded_time::duration::Microseconds;
+use embedded_time::fixed_point::FixedPoint;
 
 /// `Position` represents the total "distance" ridden by the motor.
 /// For maximal precision, it is split into the counter of revolutions and the current angle.
-#[derive(Copy, Clone, Default)]
+#[derive(Copy, Clone)]
 pub struct Position {
-    pub resolution: u16,
-    pub revolutions: i32,
-    pub angle: u16,
+    resolution: u16,
+    revolutions: i32,
+    angle: u16,
 }
 
 impl Position {
@@ -26,15 +29,15 @@ impl Position {
     /// use sm4_shared::encoder::Position;
     ///
     /// let position = Position::zero(4);
-    /// assert_eq!(position.resolution, 4);
-    /// assert_eq!(position.revolutions, 0);
-    /// assert_eq!(position.angle, 0);
+    /// assert_eq!(position.get_resolution(), 4);
+    /// assert_eq!(position.get_revolutions(), 0);
+    /// assert_eq!(position.get_angle(), 0);
     /// ```
     pub fn zero(resolution: u16) -> Self {
         Self {
             resolution,
             revolutions: 0,
-            angle: 0
+            angle: 0,
         }
     }
 
@@ -51,50 +54,142 @@ impl Position {
     /// use sm4_shared::encoder::Position;
     /// let position = Position::new(4, 5, 2);
     ///
-    /// assert_eq!(position.resolution, 4);
-    /// assert_eq!(position.revolutions, 5);
-    /// assert_eq!(position.angle, 2);
+    /// assert_eq!(position.get_resolution(), 4);
+    /// assert_eq!(position.get_revolutions(), 5);
+    /// assert_eq!(position.get_angle(), 2);
     ///
     /// let invalid_position = Position::new(4, 1, 7);
-    /// assert_eq!(invalid_position.resolution, 4);
-    /// assert_eq!(invalid_position.revolutions, 2);
-    /// assert_eq!(invalid_position.angle, 3);
+    /// assert_eq!(invalid_position.get_revolutions(), 2);
+    /// assert_eq!(invalid_position.get_angle(), 3);
+    ///
+    /// let invalid_position = Position::new(4, -1, 2);
+    /// assert_eq!(invalid_position.get_revolutions(), -1);
+    /// assert_eq!(invalid_position.get_angle(), 2);
     /// ```
     pub fn new(resolution: u16, revolutions: i32, angle: u16) -> Self {
         Self {
             resolution,
             revolutions: revolutions + (angle / resolution) as i32,
-            angle: angle % resolution
+            angle: angle % resolution,
         }
     }
 
-    fn as_increments(&self) -> i32 {
+    pub fn get_resolution(&self) -> u16 {
+        self.resolution
+    }
+
+    pub fn get_revolutions(&self) -> i32 {
+        self.revolutions
+    }
+
+    pub fn get_angle(&self) -> u16 {
+        self.angle
+    }
+
+    /// Returns the position as number of increments, this is useful for precise control.
+    /// # Examples
+    /// ```
+    /// use sm4_shared::encoder::Position;
+    ///
+    /// let position = Position::new(4, 1, 2);
+    /// assert_eq!(position.as_increments(), 6);
+    ///
+    /// let position = Position::new(4, -1, 2);
+    /// assert_eq!(position.as_increments(), -2);
+    /// ```
+    pub fn as_increments(&self) -> i32 {
         self.revolutions * self.resolution as i32 + self.angle as i32
+    }
+
+    /// Returns number of revolutions as float with the angle embedded after the decimal point
+    /// # Examples
+    /// ```
+    /// use sm4_shared::encoder::Position;
+    ///
+    /// let position = Position::new(4, 1, 2);
+    /// assert_eq!(position.get_relative_revolutions(), 1.5);
+    ///
+    /// let position = Position::new(4, -1, 2);
+    /// assert_eq!(position.get_relative_revolutions(), -0.5);
+    /// ```
+    pub fn get_relative_revolutions(&self) -> f32 {
+        self.revolutions as f32 + self.angle as f32 / self.resolution as f32
     }
 }
 
-use core::ops::Add;
-use embedded_time::duration::Microseconds;
-use embedded_time::fixed_point::FixedPoint;
+impl AddAssign<i32> for Position {
+    /// Adds increments to position
+    /// # Examples
+    /// ```
+    /// use sm4_shared::encoder::Position;
+    ///
+    /// let mut position = Position::zero(4);
+    /// position += 1;
+    ///
+    /// assert_eq!(position.as_increments(), 1);
+    ///
+    /// position += 5;
+    /// assert_eq!(position.as_increments(), 6);
+    ///
+    /// position += -2;
+    /// assert_eq!(position.as_increments(), 4);
+    /// ```
+    fn add_assign(&mut self, rhs: i32) {
+        let added_revolutions = rhs / self.resolution as i32;
+        let added_angle = rhs % self.resolution as i32;
+
+        let mut new_revolutions = self.revolutions + added_revolutions;
+        let mut new_angle = added_angle + self.angle as i32;
+
+        if new_angle.abs() as i32 >= self.resolution as i32 {
+            new_revolutions += new_angle.signum();
+            new_angle = new_angle % self.resolution as i32;
+        }
+
+        if new_angle < 0 {
+            new_revolutions -= 1;
+            new_angle += self.resolution as i32;
+        }
+
+        self.revolutions = new_revolutions;
+        self.angle = new_angle as u16;
+    }
+}
+
+impl SubAssign<i32> for Position {
+    fn sub_assign(&mut self, rhs: i32) {
+        *self += -rhs;
+    }
+}
 
 /// Represents speed measured by the encoder
-#[derive(Copy, Clone, Default)]
+#[derive(Copy, Clone)]
 pub struct Speed {
     /// revolutions per second
     pub rps: f32,
 }
 
 impl Speed {
-    fn from_position_diff(current: &Position, past: &Position, period: Microseconds) -> Self {
+    pub fn zero() -> Self {
+        Self { rps: 0.0 }
+    }
+
+    pub fn new(rps: f32) -> Self {
+        Self { rps }
+    }
+
+    fn from_positions(current: &Position, past: &Position, period: Microseconds) -> Self {
         let resolution = current.resolution as f32;
         let diff = (current.as_increments() - past.as_increments()) as f32;
         let rps = diff / resolution * 1.0e6 / *period.integer() as f32;
-        Self {
-            rps
-        }
+        Self { rps }
     }
 }
 
+/// A trait abstracting common encoder functionality.
+/// It is suitable for both incremental and absolute encoders.
+/// It is designed so its [`Self::sample()`] shall be periodically called with known fixed period,
+/// which allows for speed calculations.
 pub trait Encoder {
     fn get_speed(&self) -> Speed;
     fn get_position(&self) -> Position;
@@ -122,7 +217,7 @@ mod tests {
 
     impl Encoder for MockEncoder {
         fn get_speed(&self) -> Speed {
-            unimplemented!()
+            self.current_speed
         }
 
         fn get_position(&self) -> Position {
@@ -131,20 +226,17 @@ mod tests {
 
         fn reset_position(&mut self) -> Position {
             let past = self.current_position;
-            self.current_position = Position::default();
-            self.current_speed = Speed::default();
+            self.current_position = Position::zero(ENCODER_RESOLUTION);
+            self.current_speed = Speed::zero();
             past
         }
 
         fn sample(&mut self) {
             let past = self.current_position;
-            if self.clockwise {
-                self.current_position.angle += 1;
-            } else {
-                self.current_position.angle -= 1;
-            }
+            self.current_position += if self.clockwise { 1 } else { -1 };
 
-            self.current_speed = Speed::from_position_diff(&self.current_position, &past, self.sampling_period);
+            self.current_speed =
+                Speed::from_positions(&self.current_position, &past, self.sampling_period);
         }
 
         fn notify_direction_changed(&mut self, clockwise: bool) {
@@ -166,36 +258,38 @@ mod tests {
             angle: 1,
         };
 
-        let speed = Speed::from_position_diff(&position2, &position1, Microseconds(10));
+        let speed = Speed::from_positions(&position2, &position1, Microseconds(10));
         assert_eq!(speed.rps, 25000.0);
 
-        let speed = Speed::from_position_diff(&position1, &position2, Microseconds(10));
+        let speed = Speed::from_positions(&position1, &position2, Microseconds(10));
         assert_eq!(speed.rps, -25000.0);
 
-        let position1 = Position {
-            resolution: ENCODER_RESOLUTION,
-            revolutions: 0,
-            angle: ENCODER_RESOLUTION - 1,
-        };
+        let position1 = Position::new(ENCODER_RESOLUTION, 0, ENCODER_RESOLUTION - 1);
 
-        let position2 = Position {
-            resolution: ENCODER_RESOLUTION,
-            revolutions: 1,
-            angle: 0,
-        };
+        let position2 = Position::new(ENCODER_RESOLUTION, 1, 0);
 
-        let speed = Speed::from_position_diff(&position2, &position1, Microseconds(10));
+        let speed = Speed::from_positions(&position2, &position1, Microseconds(10));
         assert_eq!(speed.rps, 25000.0);
-
-        let position = Position::new(4, 5, 2);
-
-        assert_eq!(position.resolution, 4);
-        assert_eq!(position.revolutions, 5);
-        assert_eq!(position.angle, 2);
     }
 
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn position_manipulation() {
+        let mut position = Position::zero(ENCODER_RESOLUTION);
+        position += 6;
+        assert_eq!(position.revolutions, 1);
+        assert_eq!(position.angle, 2);
+
+        position += -2;
+        assert_eq!(position.revolutions, 1);
+        assert_eq!(position.angle, 0);
+
+        position += -1;
+        assert_eq!(position.revolutions, 0);
+        assert_eq!(position.angle, 3);
+
+        position -= 5;
+        assert_eq!(position.revolutions, -1);
+        assert_eq!(position.angle, 2);
+        assert_eq!(position.as_increments(), -2);
     }
 }
