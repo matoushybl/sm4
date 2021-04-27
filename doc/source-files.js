e23e599ec654f512a994ddfe939fee1e465b772c
matoushybl <hyblmatous@gmail.com>
2021-04-27 20:38:03 +0000

    Deploying to gh-pages from  @ deaea9055cd6f7461615597ff96d0fcaf4ebaaea 🚀

--- doc/source-files.js
+++ doc/source-files.js
@@ -1,4 +1,4 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["main"] = {"name":"","files":["main.rs"]};
-sourcesIndex["sm4_firmware"] = {"name":"","dirs":[{"name":"blocks","files":["current_reference.rs","eeprom.rs","flash.rs","gpio.rs","leds.rs","mod.rs","monitoring.rs","step_counter.rs","step_timer.rs","usb.rs"]},{"name":"protocol","files":["canopen.rs","mod.rs"]}],"files":["board.rs","can.rs","lib.rs","object_dictionary.rs","sm4.rs","state.rs"]};
+sourcesIndex["sm4_firmware"] = {"name":"","dirs":[{"name":"blocks","files":["current_reference.rs","eeprom.rs","flash.rs","gpio.rs","leds.rs","mod.rs","monitoring.rs","step_counter.rs","step_timer.rs","usb.rs"]},{"name":"protocol","files":["canopen.rs","mod.rs"]}],"files":["board.rs","can.rs","i2c.rs","lib.rs","object_dictionary.rs","sm4.rs","state.rs"]};
 createSourceSidebar();
