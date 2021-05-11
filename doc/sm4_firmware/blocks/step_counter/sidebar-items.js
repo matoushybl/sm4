7454aaa5dc6ea486774a9f1a11081d209826398a
matoushybl <hyblmatous@gmail.com>
2021-05-11 20:42:08 +0000

    Deploying to gh-pages from  @ a1fe0f77b016648798adeb8f9d07374f4493903a 🚀

--- doc/sm4_firmware/blocks/step_counter/sidebar-items.js
+++ doc/sm4_firmware/blocks/step_counter/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["StepCounterEncoder",""]],"trait":[["Counter",""]]});
+initSidebarItems({"struct":[["StepCounterEncoder",""]],"trait":[["Counter","Trait used to abstract STEP pulse and other counters."]]});
