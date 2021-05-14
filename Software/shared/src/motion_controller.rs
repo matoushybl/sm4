62361428cbeb435a1f3167783316eb0b56a87cc1
Matous Hybl <hyblmatous@gmail.com>
2021-05-14 18:47:50 +0200

    Describe motion controller.

--- Software/shared/src/motion_controller.rs
+++ Software/shared/src/motion_controller.rs
@@ -3,12 +3,18 @@ use num_traits::Float;
 
 use embedded_time::duration::Microseconds;
 
+/// Motion controller of an arbitrary axis.
+/// This motion controller expects that the target driver is controlled either in velocity or position mode.
+/// Trapezoidal ramp generator is utilized.
 pub struct AxisMotionController<D: StepperDriver, E: Encoder<RESOLUTION>, const RESOLUTION: u32> {
+    /// The target stepper motor driver, that will be controlled by this motion controller.
     driver: D,
+    /// The encoder, that will be used to provide feedback for closed loop control
     encoder: E,
     velocity_controller: PSDController,
     position_controller: PSDController,
     ramp_generator: TrapRampGen,
+    /// Variable used to store the calculated velocity action for ramp generator.
     axis_velocity_action: f32,
 }
 
@@ -27,9 +33,6 @@ impl<D: StepperDriver, E: Encoder<RESOLUTION>, const RESOLUTION: u32>
     }
 
     pub fn ramp(&mut self, global_disable: bool, dictionary: &mut dyn AxisDictionary<RESOLUTION>) {
-        self.encoder.sample();
-        dictionary.set_actual_position(self.encoder.get_position());
-
         if global_disable {
             self.axis_velocity_action = 0.0;
         }
@@ -42,8 +45,6 @@ impl<D: StepperDriver, E: Encoder<RESOLUTION>, const RESOLUTION: u32>
         if Direction::from(dictionary.actual_velocity().get_rps()) != axis_new_direction {
             self.encoder.notify_direction_changed(axis_new_direction);
         }
-        // TODO in case of using encoders, it should be read from the encoder
-        dictionary.set_actual_velocity(Velocity::new(output_frequency));
 
         self.driver.set_output_frequency(output_frequency);
         let current = if output_frequency.abs() < 0.1 {
@@ -61,6 +62,14 @@ impl<D: StepperDriver, E: Encoder<RESOLUTION>, const RESOLUTION: u32>
         global_disable: bool,
         dictionary: &mut dyn AxisDictionary<RESOLUTION>,
     ) {
+        self.encoder.sample();
+        dictionary.set_actual_position(self.encoder.get_position());
+        dictionary.set_actual_velocity(if dictionary.velocity_feedback_control_enabled() {
+            self.encoder.get_velocity()
+        } else {
+            Velocity::new(output_frequency)
+        });
+
         let target_velocity = if dictionary.enabled() && !global_disable {
             match dictionary.mode() {
                 AxisMode::Velocity => dictionary.target_velocity(),
